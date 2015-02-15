2fc908d0ab213484bdff8b403edff897ab2ddc39
yuanxiaodu <yuanxiaodu@gmail.com>
2015-02-15 15:11:01 +0800

    init

--- routes/manage.js
+++ routes/manage.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* manage home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('./manage/login', { title: '后台管理登录' });
 });
 
 module.exports = router;

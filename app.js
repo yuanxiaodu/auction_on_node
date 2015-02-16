034b23e0280e871b993af6fde9f6a9731cf3242b
yuanxiaodu <yuanxiaodu@gmail.com>
2015-02-16 22:06:47 +0800

    init

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//routes
 var manage = require('./routes/manage');
 
 var app = express();
@@ -13,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/manage', manage);
 
 // catch 404 and forward to error handler
--- routes/manage.js
+++ routes/manage.js
@@ -4,27 +4,27 @@ var router = express.Router();
 var User = require('../models/User');
 
 /* manage home page. */
-router.get('/', function (req, res) {
-    res.render('./manage/login', {title: '后台管理登录'});
+router.get(/^\/(login)?$/, function (req, res) {
+    res.render('manage/login', {title: '后台管理登录'});
 });
 
 router.post('/login', function (req, res) {
     var username = req.param('username');
     var password = req.param('password');
-    User.find(function (err, obj) {
-        res.send({title: '用户名或密码错误', error: '用户名或密码错误'});
+    User.findOne({username: username}, function (err, user) {
+        if (err) {
+            console.log(err)
+        } else {
+            if (!user || user.password != password) {
+                res.send({code:'failed', msg:'用户名或密码错误'});
+            } else {
+                res.send({code:'success'});
+            }
+        }
     });
-
-
-    //if (User.validatePassword(username, req.param('password'))) {
-    //    User.findOne({username: username}, function (err, adventure) {
-    //        console.log(err);
-    //        console.log(adventure);
-    //        res.render('./manage/user')
-    //    })
-    //} else {
-    //    res.render('./manage/login', {title: '用户名或密码错误', msg: '用户名或密码错误'});
-    //}
+});
+router.get('/user', function (req, res) {
+    res.render('manage/user');
 });
 
 module.exports = router;

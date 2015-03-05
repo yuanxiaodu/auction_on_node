2fff706524f9964f70eb6cfde07d5124ced96685
yuanxiaodu <yuanxiaodu@gmail.com>
2015-03-06 01:53:16 +0800

    init

--- models/Product.js
+++ models/Product.js
@@ -25,16 +25,4 @@ var ProductSchema = new Schema({
     }]
 });
 
-// Ensure virtual fields are serialised.
-ProductSchema.set('toObject', {
-    virtuals: true
-});
-
-schema.set('toJSON', {
-    transform: function (doc, ret, options) {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.__v;
-    }
-});
 module.exports = mongodb.mongoose.model("Product", ProductSchema);
--- routes/manage/product.js
+++ routes/manage/product.js
@@ -119,11 +119,10 @@ router.get('/modifyProduct/:id/:status', function (req, res) {
 
 router.get('/modifyProduct/:id', function (req, res) {
     var id = req.params.id
-    Product.findById(id, function (err, product) {
+    Product.findById(id, {}, function (err, product) {
         if (err) {
             console.log(err)
         } else {
-            console.log(product)
             res.render('manage/modifyProduct', {title: '编辑商品', product: product, user: req.session.user});
         }
     })
@@ -131,13 +130,105 @@ router.get('/modifyProduct/:id', function (req, res) {
 
 router.post('/modifyProduct/:id', function (req, res) {
     var id = req.params.id
-    Product.findById(id, function (err, product) {
+    var files = req.files.pictures,
+        startPrice = req.body.startPrice,
+        markup = req.body.markup,
+        start = new Date(req.body.start),
+        end = new Date(req.body.end),
+        catalog = req.body.catalog,
+        description = req.body.description;
+    if (files) {
+        if (!files.length) {
+            files = [files]
+        }
+    }
+    var preg = /^[1-9]{1}[0-9]{0,10}[.]{0,1}[0-9]{0,2}$/
+    if (!preg.test(startPrice) || !preg.test(markup)) {
+        if (files) {
+            for (var index in files) {
+                fs.unlinkSync(files[index].path)
+            }
+        }
+        res.send({code: 'failed', msg: '价格格式为 XX.XX'})
+    } else if (start > end) {
+        if (files) {
+            for (var index in files) {
+                fs.unlinkSync(files[index].path)
+            }
+        }
+        res.send({code: 'failed', msg: '起拍时间不能晚于结束时间'})
+    } else {
+        if (files) {
+            for (var index in files) {
+                fs.renameSync(files[index].path, fs.realpathSync('../public/images/') + '/' + files[index].name)
+                Product.findByIdAndUpdate(id, {
+                    $push: {
+                        pictures: {
+                            originalname: files[index].originalname,
+                            name: files[index].name
+                        }
+                    }
+                }, function (err) {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+            }
+        }
+        Product.findByIdAndUpdate(id, {
+            startPrice: startPrice,
+            markup: markup,
+            start: start,
+            end: end,
+            catalog: catalog,
+            description: description,
+            lastModify: new Date,
+            modifier: req.session.user.username
+        }, function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send({code: 'success'})
+            }
+        })
+
+    }
+
+})
+
+router.post('/deletePicture/:id', function (req, res) {
+    var id = req.params.id
+    var name = req.body.key
+    Product.findByIdAndUpdate(id, {
+        $pull: {pictures: {name: name}},
+        lastModify: new Date,
+        modifier: req.session.user.username
+    }, function (err) {
         if (err) {
             console.log(err)
         } else {
+            fs.unlinkSync(fs.realpathSync('../public/images/') + '/' + name)
             res.send({code: 'success'})
         }
     })
+})
+
+router.post('/uploadPicture/:id', function (req, res) {
+    var id = req.params.id
+    var picture = req.files.pictures
+    Product.findByIdAndUpdate(id, {
+        $push: {pictures: {originalname: picture.originalname, name: picture.name}},
+        lastModify: new Date,
+        modifier: req.session.user.username
+    }, function (err, product) {
+        if (err) {
+            console.log(err)
+        } else {
+            fs.renameSync(picture.path, fs.realpathSync('../public/images/') + '/' + picture.name)
+            res.send({code: 'success', picture: {originalname: picture.originalname, name: picture.name}})
+        }
+    })
+
 })
 
 module.exports = router;

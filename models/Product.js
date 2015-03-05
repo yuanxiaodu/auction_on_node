0866785ec50b273c8d117138dc233a77b4734061
yuanxiaodu <yuanxiaodu@gmail.com>
2015-03-05 19:21:08 +0800

    init

--- models/Product.js
+++ models/Product.js
@@ -29,4 +29,12 @@ var ProductSchema = new Schema({
 ProductSchema.set('toObject', {
     virtuals: true
 });
+
+schema.set('toJSON', {
+    transform: function (doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+});
 module.exports = mongodb.mongoose.model("Product", ProductSchema);
